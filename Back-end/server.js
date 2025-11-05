f2b58d60e735c81c956110827b63bae235f6ceb3
Boss Budget Proyect <bossbudgetproyect@gmail.com>
2025-11-05 04:53:40 +0000

    Aplicativo funciona el main, trabajar en ramas aparte

--- Back-end/server.js
+++ Back-end/server.js
@@ -1,11 +1,9 @@
 require('dotenv').config();
 const app = require('./src/app');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
     console.log(`🚀 BossBudget API running on port ${PORT}`);
     console.log(`📊 Environment: ${process.env.NODE_ENV || 'development'}`);
 });
-
-// https://automatic-journey-5g5wgjj75gjq2p6pg-3000.app.github.dev/
